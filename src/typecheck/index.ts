2e63cb0458e621827ecc4bb90f65985517dd6e76
Isa(yunjongryu) <yunjonglyu@gmail.com>
2023-01-11 06:29:05 +0900

    feat: BIND PKG

--- src/typecheck/index.ts
+++ src/typecheck/index.ts
@@ -1,11 +1,13 @@
 import isFunction from './isFunction/isFunction';
 import isArray from './isArray/isArray';
 import isString from './isString/isString';
+import isNumber from './isNumber/isNumber';
 
 const TypeCheck = Object.freeze({
   isFunction,
   isArray,
   isString,
+  isNumber,
 });
 
 export default TypeCheck;
