58add914ccb1c2e917f993badd48077883e2988b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-08-24 23:44:52 +0900

    feat(API): GET PLATFORM

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import debounce from './debounce/debounce';
 import throttle from './throttle/throttle';
+import getPlatfrom from './getPlatform/getPlatform';
 
 const Api = Object.freeze({
   debounce,
   throttle,
+  getPlatfrom,
 });
 
 export default Api;
